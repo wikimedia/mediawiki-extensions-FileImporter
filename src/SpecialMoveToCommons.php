f8411e5bf01e01c2fa69eee0c81bf1a7043acaf2
addshore <addshorewiki@gmail.com>
2017-02-02 18:11:52 +0000

    Add special page shell
    Change-Id: I658531e6014c2125fc616297ed2cd781f073e9b7

---
+++ src/SpecialMoveToCommons.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MoveToCommons;
+
+use SpecialPage;
+
+class SpecialMoveToCommons extends SpecialPage {
+
+	public function __construct() {
+		parent::__construct( 'MoveToCommons' );
+	}
+
+	public function execute( $subPage ) {
+		$out = $this->getOutput();
+		$out->setPageTitle( new \Message( 'movetocommons-specialpage' ) );
+
+		$rawUrl = $out->getRequest()->getVal( 'clientUrl' );
+		$parsedUrl = wfParseUrl( $rawUrl );
+		if ( $parsedUrl === false ) {
+			$this->showUrlEntryPage( $rawUrl );
+		} elseif ( !$this->urlAllowed( $parsedUrl ) ) {
+			$this->showUrlNotAllowedPage();
+		} else {
+			$this->showImportPage( $parsedUrl );
+		}
+	}
+
+	/**
+	 * @param string[] $parsedUrl return of wfParseUrl
+	 *
+	 * @return bool
+	 */
+	private function urlAllowed( array $parsedUrl ) {
+		// TODO decide if this URL is allowed. ie, is it a wikimedia project & a mediawiki wiki
+		return true;
+	}
+
+	/**
+	 * @param string $rawUrl
+	 */
+	private function showUrlEntryPage( $rawUrl ) {
+		// TODO show a simple special page containing 1 input box that a URL can be pasted into
+	}
+
+	private function showUrlNotAllowedPage() {
+		// TODO show an error page stating that the URL entered is not allowed for whatever reason!
+	}
+
+	/**
+	 * @param string[] $parsedUrl return of wfParseUrl
+	 */
+	private function showImportPage( array $parsedUrl ) {
+		// TODO show the details of the import
+	}
+
+}
