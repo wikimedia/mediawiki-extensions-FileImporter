a8405e7655ad41895d38028d49eb75a6a62cdae6
addshore <addshorewiki@gmail.com>
2017-07-04 14:22:49 +0100

    Add ImportOperations
    ImportOperations have a concept of rolling back.
    Lots of logic has been removed from Importer and put
    into these operations (with more to come).
    The rollback logic is not actually implemented yet
    but the structure is there.
    
    Bug: T147451
    Change-Id: I109520afdb96c87fb196c06e4746d022d973ea88

---
+++ src/Data/ImportOperations.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace FileImporter\Data;
+
+use FileImporter\Interfaces\ImportOperation;
+use RuntimeException;
+
+class ImportOperations implements ImportOperation {
+
+	/**
+	 * @var ImportOperation[]
+	 */
+	private $importOperations = [];
+
+	/**
+	 * @var int the state of this object, one of the class constants
+	 */
+	private $state = self::BUILDING;
+
+	const BUILDING = 0;
+	const PREPARE_RUN = 1;
+	const COMMIT_RUN = 2;
+	const ROLLBACK_RUN = 3;
+
+	public function add( ImportOperation $importOperation ) {
+		$this->importOperations[] = $importOperation;
+	}
+
+	/**
+	 * @param int $expectedState one of the class constants
+	 */
+	private function throwExceptionOnBadState( $expectedState ) {
+		if ( $this->state !== $expectedState ) {
+			throw new RuntimeException( __CLASS__ . ' methods run out of order' );
+		}
+	}
+
+	/**
+	 * Method to prepare an operation. This will not commit anything to any persistent storage.
+	 * For example, this could make API calls and validate data.
+	 * @return bool success
+	 */
+	public function prepare() {
+		$this->throwExceptionOnBadState( self::BUILDING );
+		$this->state = self::PREPARE_RUN;
+		foreach ( $this->importOperations as $importOperation ) {
+			if ( !$importOperation->prepare() ) {
+				// TODO log?
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Commit this operation to persistent storage.
+	 * @return bool success
+	 */
+	public function commit() {
+		$this->throwExceptionOnBadState( self::PREPARE_RUN );
+		$this->state = self::COMMIT_RUN;
+		foreach ( $this->importOperations as $importOperation ) {
+			if ( !$importOperation->commit() ) {
+				// TODO log?
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Rollback this operation to persistent storage.
+	 * @return bool success
+	 */
+	public function rollback() {
+		$this->throwExceptionOnBadState( self::COMMIT_RUN );
+		$this->state = self::ROLLBACK_RUN;
+		foreach ( $this->importOperations as $importOperation ) {
+			if ( !$importOperation->rollback() ) {
+				// TODO log?
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
---
+++ src/Operations/FileRevisionFromRemoteUrl.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace FileImporter\Operations;
+
+use FileImporter\Data\FileRevision;
+use FileImporter\Interfaces\ImportOperation;
+use FileImporter\Services\Http\HttpRequestExecutor;
+use FileImporter\Services\UploadBase\UploadBaseFactory;
+use FileImporter\Services\WikiRevisionFactory;
+use Http;
+use TempFSFile;
+use Title;
+use WikiRevision;
+
+class FileRevisionFromRemoteUrl implements ImportOperation {
+
+	/**
+	 * @var Title
+	 */
+	private $plannedTitle;
+
+	/**
+	 * @var FileRevision
+	 */
+	private $fileRevision;
+
+	/**
+	 * @var HttpRequestExecutor
+	 */
+	private $httpRequestExecutor;
+
+	/**
+	 * @var WikiRevisionFactory
+	 */
+	private $wikiRevisionFactory;
+
+	/**
+	 * @var UploadBaseFactory
+	 */
+	private $uploadBaseFactory;
+
+	/**
+	 * @var WikiRevision|null
+	 */
+	private $wikiRevision;
+
+	public function __construct(
+		Title $plannedTitle,
+		FileRevision $fileRevision,
+		HttpRequestExecutor $httpRequestExecutor,
+		WikiRevisionFactory $wikiRevisionFactory,
+		UploadBaseFactory $uploadBaseFactory
+	) {
+		$this->plannedTitle = $plannedTitle;
+		$this->fileRevision = $fileRevision;
+		$this->httpRequestExecutor = $httpRequestExecutor;
+		$this->wikiRevisionFactory = $wikiRevisionFactory;
+		$this->uploadBaseFactory = $uploadBaseFactory;
+	}
+
+	/**
+	 * Method to prepare an operation. This will not commit anything to any persistent storage.
+	 * For example, this could make API calls and validate data.
+	 * @return bool success
+	 */
+	public function prepare() {
+		$fileUrl = $this->fileRevision->getField( 'url' );
+		if ( !Http::isValidURI( $fileUrl ) ) {
+			// invalid URL detected
+			return false;
+		}
+
+		$tmpFile = TempFSFile::factory( 'fileimporter_', '', wfTempDir() );
+		$tmpFile->bind( $this );
+
+		$this->httpRequestExecutor->executeAndSave( $fileUrl, $tmpFile->getPath() );
+
+		$this->wikiRevision = $this->wikiRevisionFactory->newFromFileRevision(
+			$this->fileRevision,
+			$tmpFile->getPath(),
+			true
+		);
+
+		$base = $this->uploadBaseFactory->newValidatingUploadBase(
+			$this->plannedTitle,
+			$this->wikiRevision->getFileSrc()
+		);
+		return $base->validateTitle() === true && $base->validateFile() === true;
+	}
+
+	/**
+	 * Commit this operation to persistent storage.
+	 * @return bool success
+	 */
+	public function commit() {
+		return $this->wikiRevision->importUpload();
+	}
+
+	/**
+	 * Rollback this operation to persistent storage.
+	 * @return bool success
+	 */
+	public function rollback() {
+		// TODO: Implement rollback() method.
+		return false;
+	}
+
+}
