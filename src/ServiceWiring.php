470a99c849cd2e33b8110559ac449ecdd2d12c38
Andrew-WMDE <andrew.kostka@wikimedia.de>
2018-01-08 12:30:52 +0100

    Check each file revision size against max allowed before previewing.
    Bug: T182062
    Change-Id: I33c89325f802e676e7865f0ba17b539065194f1a

--- src/ServiceWiring.php
+++ src/ServiceWiring.php
@@ -107,13 +107,15 @@ return [
 		$httpApiLookup = $services->getService( 'FileImporterMediaWikiHttpApiLookup' );
 		$httpRequestExecutor = $services->getService( 'FileImporterHttpRequestExecutor' );
 		$logger = LoggerFactory::getInstance( 'FileImporter' );
+		$maxFileSize = UploadBase::getMaxUploadSize( 'import' );
 
 		$site = new SourceSite(
 			new AnyMediaWikiFileUrlChecker(),
 			new Remote\MediaWiki\ApiDetailRetriever(
 				$httpApiLookup,
 				$httpRequestExecutor,
-				$logger
+				$logger,
+				$maxFileSize
 			),
 			new Remote\MediaWiki\RemoteApiImportTitleChecker(
 				$httpApiLookup,
