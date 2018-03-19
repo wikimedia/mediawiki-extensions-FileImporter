c9d9268f0dc4bf491186eae95d70212c19b67cd1
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2018-03-19 15:23:05 +0100

    Remove default "return true" from hook handlers
    This is the default for a long time now. There is no difference between
    returning true and void.
    
    Change-Id: I9bac8b865a8ec42c4635ded47fa1e0b69608f6af

--- src/FileImporterHooks.php
+++ src/FileImporterHooks.php
@@ -12,13 +12,11 @@ class FileImporterHooks {
 	 * Add FileImporter username to the list of reserved ones for
 	 * replacing suppressed usernames in certain revisions
 	 *
-	 * @param array &$reservedUsernames
-	 * @return bool
+	 * @param string[] &$reservedUsernames
 	 */
 	public static function onUserGetReservedNames( &$reservedUsernames ) {
 		global $wgFileImporterAccountForSuppressedUsername;
 		$reservedUsernames[] = $wgFileImporterAccountForSuppressedUsername;
-		return true;
 	}
 
 }
