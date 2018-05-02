6eddd0a4de603f4ed4ad271002c26b8829eaca5f
WMDE-Fisch <christoph.jauera@wikimedia.de>
2018-05-02 15:13:02 +0200

    Add tests for FileChunkSaver
    Bug: T190825
    Change-Id: I8a79cdb4d7b2b72073ee49c1ddc18b1546ebb05e

---
+++ tests/phpunit/Services/Http/FileChunkSaverTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace FileImporter\Services\Http\Test;
+
+use FileImporter\Services\Http\FileChunkSaver;
+use RuntimeException;
+
+/**
+ * @covers \FileImporter\Services\Http\FileChunkSaver
+ *
+ * @license GPL-2.0-or-later
+ * @author Christoph Jauera <christoph.jauera@wikimedia.de>
+ */
+class FileChunkSaverTest extends \MediaWikiTestCase {
+
+	public function provideSaveFileChunk() {
+		return [
+			"save less bytes then allowed" =>
+				[ str_repeat( 'x', 10 ), 20, 10 ],
+			"save exactly the bytes allowed" =>
+				[ str_repeat( 'x', 20 ), 20, 20 ],
+			"save empty buffer" =>
+				[ '', 20, 0 ],
+			"empty buffer and maxbytes is zero" =>
+				[ '', 0, 0 ],
+			"save more bytes then allowed" =>
+				[ str_repeat( 'x', 20 ), 1, RuntimeException::class ],
+			"maxbytes is zero" =>
+				[ str_repeat( 'x', 20 ), 0, RuntimeException::class ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideSaveFileChunk
+	 */
+	public function testSaveFileChunk( $buffer, $maxBytes, $expectedResult ) {
+		$saver = $this->createChunkSaver( $maxBytes );
+
+		if ( !is_int( $expectedResult ) ) {
+			$this->setExpectedException( $expectedResult );
+		}
+
+		$this->assertSame( $expectedResult, $saver->saveFileChunk( null, $buffer ) );
+	}
+
+	private function createChunkSaver( $maxBytes ) {
+		$file = $this->getNewTempFile();
+		return new FileChunkSaver( $file, $maxBytes );
+	}
+
+}
