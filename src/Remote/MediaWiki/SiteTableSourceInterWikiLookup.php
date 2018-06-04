c870c05b210a7e4b8d308919c30d7b1f88483254
WMDE-Fisch <christoph.jauera@wikimedia.de>
2018-06-04 17:53:44 +0200

    Add basic logging to SiteTableSourceInterWikiLookup
    Bug: T192376
    Change-Id: Ib0bdaa78c51a409a6096915f7a182e1deaa4041a

--- src/Remote/MediaWiki/SiteTableSourceInterWikiLookup.php
+++ src/Remote/MediaWiki/SiteTableSourceInterWikiLookup.php
@@ -4,6 +4,7 @@ namespace FileImporter\Remote\MediaWiki;
 
 use FileImporter\Data\SourceUrl;
 use FileImporter\Interfaces\SourceInterWikiLookup;
+use Psr\Log\LoggerInterface;
 
 /**
  * This SourceInterWikiLookup implementation will allow interwiki references
@@ -14,26 +15,50 @@ use FileImporter\Interfaces\SourceInterWikiLookup;
  */
 class SiteTableSourceInterWikiLookup implements SourceInterWikiLookup {
 
+	/**
+	 * @var SiteTableSiteLookup
+	 */
 	private $siteTableSiteLookup;
 
+	/**
+	 * @var LoggerInterface
+	 */
+	private $logger;
+
 	public function __construct(
-		SiteTableSiteLookup $siteTableSiteLookup
+		SiteTableSiteLookup $siteTableSiteLookup,
+		LoggerInterface $logger
 	) {
 		$this->siteTableSiteLookup = $siteTableSiteLookup;
+		$this->logger = $logger;
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function getPrefix( SourceUrl $sourceUrl ) {
-		$site = $this->siteTableSiteLookup->getSite( $sourceUrl->getParsedUrl()['host'] );
+		$host = $sourceUrl->getParsedUrl()['host'];
+		$site = $this->siteTableSiteLookup->getSite( $host );
 		$prefix = '';
 		if ( $site === null ) {
+			$this->logger->warning(
+				'No site found in site table.',
+				[
+					'host' => $host
+				]
+			);
 			return $prefix;
 		}
 
 		$interWikiIds = $site->getInterwikiIds();
 		if ( empty( $interWikiIds ) ) {
+			$this->logger->warning(
+				'No interWikiIds for site.',
+				[
+					'host' => $host,
+					'siteId' => $site->getGlobalId()
+				]
+			);
 			return $prefix;
 		}
 
