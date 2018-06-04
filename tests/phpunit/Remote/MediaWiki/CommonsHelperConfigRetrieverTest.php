5b62d5098c8438e5e9557dea880fa270346e4386
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2018-06-04 18:48:09 +0200

    Add basic happy test for CommonsHelperConfigRetriever
    I plan to continue working on this in later patches. Feel free to merge
    this patch as it is now.
    
    Bug: T193615
    Change-Id: Icdcb55c7210330a4dd57f6f21f7e76ebf10165c0

---
+++ tests/phpunit/Remote/MediaWiki/CommonsHelperConfigRetrieverTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace FileImporter\Tests\Remote\MediaWiki;
+
+use FileImporter\Data\SourceUrl;
+use FileImporter\Remote\MediaWiki\CommonsHelperConfigRetriever;
+use FileImporter\Services\Http\HttpRequestExecutor;
+
+/**
+ * @covers \FileImporter\Remote\MediaWiki\CommonsHelperConfigRetriever
+ */
+class CommonsHelperConfigRetrieverTest extends \PHPUnit\Framework\TestCase {
+	use \PHPUnit4And6Compat;
+
+	// TODO: Test the special www.mediawiki code-path
+	// TODO: Test incompatible URLs
+	// TODO: Test the "missing" code-path
+	// TODO: Test all kinds of failures
+
+	public function testSuccess() {
+		$sourceUrl = new SourceUrl( '//de.wikipedia.org/wiki/Example.svg' );
+
+		$request = $this->createMock( \MWHttpRequest::class );
+		$request->method( 'getContent' )
+			->willReturn( json_encode( [
+				'query' => [
+					'pages' => [
+						[
+							'revisions' => [
+								[ 'content' => '<WIKITEXT>' ]
+							],
+						],
+					],
+				],
+			] ) );
+
+		$requestExecutor = $this->createMock( HttpRequestExecutor::class );
+		$requestExecutor->method( 'execute' )
+			->willReturn( $request );
+
+		$retriever = new CommonsHelperConfigRetriever( $sourceUrl, $requestExecutor );
+
+		$this->assertTrue( $retriever->retrieveConfiguration() );
+		$this->assertSame(
+			'https://meta.wikimedia.org/wiki/CommonsHelper2/Data_de.wikipedia',
+			$retriever->getConfigWikiUrl()
+		);
+		$this->assertSame( '<WIKITEXT>', $retriever->getConfigWikiText() );
+	}
+
+}
