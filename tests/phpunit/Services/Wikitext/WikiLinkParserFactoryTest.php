de40d1796f203ba50464033c3fe0365366c6abaf
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2019-05-23 15:37:52 +0200

    Add missing tests for WikiLinkParserFactory
    Change-Id: Ib96af76b8d8a6535d35ee3395477cc42e293665a

---
+++ tests/phpunit/Services/Wikitext/WikiLinkParserFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace FileImporter\Tests\Services\Wikitext;
+
+use FileImporter\Services\Wikitext\WikiLinkParserFactory;
+use Language;
+
+/**
+ * Note this test (intentionally) uses actual Language objects, which does make this an integration
+ * test with MediaWiki's i18n feature set.
+ *
+ * @covers \FileImporter\Services\Wikitext\WikiLinkParserFactory
+ *
+ * @license GPL-2.0-or-later
+ * @author Thiemo Kreuz
+ */
+class WikiLinkParserFactoryTest extends \PHPUnit\Framework\TestCase {
+
+	public function testInterwikiPrefixing() {
+		$factory = new WikiLinkParserFactory();
+		$parser = $factory->getWikiLinkParser( null, 'PREFIX' );
+		$this->assertSame( '[[:PREFIX:Benutzer:Me]]', $parser->parse( '[[Benutzer:Me]]' ) );
+	}
+
+	public function testNamespaceUnlocalization() {
+		$factory = new WikiLinkParserFactory();
+		$parser = $factory->getWikiLinkParser( Language::factory( 'de' ), '' );
+		$this->assertSame( '[[User:Me]]', $parser->parse( '[[Benutzer:Me]]' ) );
+	}
+
+	public function testWikiLinkNormalization() {
+		$factory = new WikiLinkParserFactory();
+		$parser = $factory->getWikiLinkParser( Language::factory( 'de' ), 'PREFIX' );
+		$this->assertSame( '[[:PREFIX:User:Me]]', $parser->parse( '[[Benutzer:Me]]' ) );
+	}
+
+}
