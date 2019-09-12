f7c2100615c8bfe907ab27862350658527de684f
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2019-09-12 12:09:55 +0200

    Present missing/bad license errors as (unfixable) warnings
    This patch changes the behavior for these two errors:
    * "The file cannot be imported, because it is not marked with one of
      the required licences" ("fileimporter-file-missing-required-template")
    * "The file cannot be imported, because it is marked as …"
      ("fileimporter-file-contains-blocked-category-template")
    
    These two are not errors, but reflect community consensus: files like
    this should not be transfered to Commons.
    
    With this patch, the warning message is presented in yellow instead of
    red.
    
    The longer messages intentionally do not mention Wikimedia Commons, or
    links to Commons. This needs to be done per project.
    
    Bug: T228858
    Change-Id: I9e4cf9665495c749d085e13761f80ae0156f4270

--- src/Html/ErrorPage.php
+++ src/Html/ErrorPage.php
@@ -14,18 +14,19 @@ class ErrorPage extends SpecialPageHtmlFragment {
 	/**
 	 * @param string $errorMessage HTML
 	 * @param string|null $url
+	 * @param string $type Either "error" (default), or "warning"
 	 *
 	 * @return string
 	 */
-	public function getHtml( $errorMessage, $url ) {
+	public function getHtml( $errorMessage, $url, $type = 'error' ) {
 		$output = Html::rawElement(
 			'div',
-			[ 'class' => 'mw-importfile-error-banner errorbox' ],
+			[ 'class' => "mw-importfile-error-banner ${type}box" ],
 			Html::rawElement( 'p', [], $errorMessage )
 		);
 
 		if ( $url !== null ) {
-			$output .= new ButtonWidget(
+			$output .= Html::rawElement( 'br' ) . new ButtonWidget(
 				[
 					'label' => $this->msg( 'fileimporter-go-to-original-file-button' )->plain(),
 					'href' => $url,
