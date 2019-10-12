80cbfc5af0a512d6b868e4d36deec1313107df82
Umherirrender <umherirrender_de.wp@web.de>
2019-10-12 21:50:59 +0200

    phan: Remove PhanParamReqAfterOpt suppression
    Change-Id: Ia7ee35799558bb97cbf2f6325e262959b970f902

--- .phan/config.php
+++ .phan/config.php
@@ -1,8 +1,3 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-
-// Fails on parameters that are intentionally nullable, rendering this check pointless.
-$cfg['suppress_issue_types'][] = 'PhanParamReqAfterOpt';
-
-return $cfg;
+return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
