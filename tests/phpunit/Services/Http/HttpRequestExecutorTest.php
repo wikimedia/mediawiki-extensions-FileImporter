a33900f3f711464241b6d683a9334cd4d881930b
Max Semenik <maxsem.wiki@gmail.com>
2019-11-19 10:07:02 -0800

    Suppress PHPUnit 8 warnings
    Can't fix these before the migration is over.
    
    Bug: T192167
    Change-Id: I4a990e3aa5ad15e7e87f37aa0296c82f7f97999e

--- tests/phpunit/Services/Http/HttpRequestExecutorTest.php
+++ tests/phpunit/Services/Http/HttpRequestExecutorTest.php
@@ -5,6 +5,7 @@ namespace FileImporter\Tests\Services\Http;
 use FileImporter\Exceptions\HttpRequestException;
 use FileImporter\Services\Http\HttpRequestExecutor;
 use MWHttpRequest;
+use PHPUnit6And8Compat;
 use Psr\Log\LoggerInterface;
 use Status;
 
@@ -16,6 +17,7 @@ use Status;
  * @author Addshore
  */
 class HttpRequestExecutorTest extends \PHPUnit\Framework\TestCase {
+	use PHPUnit6And8Compat;
 
 	public function provideTestExecute() {
 		return [
