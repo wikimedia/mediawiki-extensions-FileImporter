2faebd6df5d32d841a1c77e4664425106375992e
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-06-12 17:08:50 +0200

    Drop redundant closures
    Same as Idf7afca.
    
    For discussion of why it's safe to remove the closures, see
    https://www.mediawiki.org/w/index.php?title=Topic:Vjvxdwz7pq8k2wn6
    
    Change-Id: Ic954a84066b93ef7bdcf71337c0f3f20fa1c1407

--- modules/ext.FileImporter.Special.js
+++ modules/ext.FileImporter.Special.js
@@ -1,13 +1,9 @@
 'use strict';
 
-$( function () {
-	var $summary = $( '.mw-importfile-import-summary' );
-	if ( !$summary.length ) {
-		return;
-	}
-
+var $summary = $( '.mw-importfile-import-summary' );
+if ( $summary.length ) {
 	OO.ui.TextInputWidget.static.infuse( $summary ).on( 'enter', function ( e ) {
 		e.preventDefault();
 		OO.ui.ButtonWidget.static.infuse( $( '.mw-importfile-import-submit' ) ).$button.click();
 	} );
-} );
+}
