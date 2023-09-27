f275cb4e53b26b074148e74229d0a47e6a4a855c
thiemowmde <thiemo.kreuz@wikimedia.de>
2023-09-27 08:27:52 +0200

    Reduce confusion around duplicate InvalidArgumentException
    There are two. The places touched in this patch are supposed to use
    the normal one. The idea of this patch is to make this much easier
    to see in the code.
    
    But there is also \FileImporter\Exceptions\InvalidArgumentException,
    a subclass of \FileImporter\Exceptions\ImportException, meant to be
    catched and presented to the user. Introduced in 2017 via I66ce495.
    We should probably rename it. But this is for a later patch.
    
    Change-Id: I8c2183d6e4ae58ec17155258641b8869d668b0b4

--- src/Data/WikitextConversions.php
+++ src/Data/WikitextConversions.php
@@ -2,8 +2,6 @@
 
 namespace FileImporter\Data;
 
-use InvalidArgumentException;
-
 /**
  * Class holding validation and cleanup rules for the file description wikitext. This class is not
  * aware of the source of these rules. They can be extracted from CommonsHelper2-compatible
@@ -62,7 +60,7 @@ class WikitextConversions {
 	 *         without any `==` syntax
 	 * ]
 	 *
-	 * @throws InvalidArgumentException if the input format misses expected fields. This should be
+	 * @throws \InvalidArgumentException if the input format misses expected fields. This should be
 	 *  unreachable, as the only provider is the CommonsHelperConfigParser.
 	 */
 	public function __construct( array $conversions ) {
@@ -96,10 +94,10 @@ class WikitextConversions {
 			}
 
 			if ( empty( $to['targetTemplate'] ) ) {
-				throw new InvalidArgumentException( "$from transfer rule misses targetTemplate" );
+				throw new \InvalidArgumentException( "$from transfer rule misses targetTemplate" );
 			}
 			if ( !isset( $to['parameters'] ) ) {
-				throw new InvalidArgumentException( "$from transfer rule misses parameters" );
+				throw new \InvalidArgumentException( "$from transfer rule misses parameters" );
 			}
 
 			$from = $this->lowercasePageName( $from );
