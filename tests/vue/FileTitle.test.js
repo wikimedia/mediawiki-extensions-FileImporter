27f693d61a192fb8d94e9fa5047f9aeb310787d2
WMDE-Fisch <christoph.jauera@wikimedia.de>
2023-11-28 09:02:30 +0100

    Codex: Test for the FileTitle validation
    Bug: T350798
    Change-Id: Id7a37bff7c833d2355f7669d9534a596b37cb614

--- tests/vue/FileTitle.test.js
+++ tests/vue/FileTitle.test.js
@@ -3,12 +3,26 @@
 const { mount } = require( '@vue/test-utils' );
 const FileTitle = require( '../../modules/components/FileTitle.vue' );
 
+const messageOutput = 'Text';
+const getNameTextOutput = 'Bar';
+
+global.mw = {
+	message: jest.fn().mockReturnValue( {
+		text: jest.fn().mockReturnValue( messageOutput )
+	} ),
+	Title: {
+		newFromUserInput: jest.fn().mockReturnValue( {
+			getNameText: jest.fn().mockReturnValue( getNameTextOutput )
+		} )
+	}
+};
+
 describe( 'Basic usage', () => {
 
 	it( 'click on title enters editing mode', async () => {
 		const wrapper = mount( FileTitle, {
 			props: {
-				fileExtension: '.png',
+				fileExtension: 'png',
 				fileTitle: 'Test'
 			}
 		} );
@@ -17,5 +31,21 @@ describe( 'Basic usage', () => {
 
 		expect( wrapper.find( 'h2' ).exists() ).toBeFalsy();
 		expect( wrapper.find( 'input' ).exists() ).toBeTruthy();
+	} );
+
+	it( 'changing title triggers normalization', async () => {
+		const wrapper = mount( FileTitle, {
+			props: {
+				fileExtension: 'png',
+				fileTitle: 'Foo'
+			}
+		} );
+
+		await wrapper.find( 'h2' ).trigger( 'click' );
+		wrapper.find( 'input' ).setValue( 'bar' );
+		await wrapper.find( 'button' ).trigger( 'click' );
+
+		expect( wrapper.find( '.cdx-message--warning' ).text() ).toBe( messageOutput );
+		expect( wrapper.find( 'h2' ).text() ).toBe( getNameTextOutput + '.png' );
 	} );
 } );
