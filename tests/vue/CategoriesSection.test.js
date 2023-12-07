1693694a41df75cb22f3d31d38d280d5283c6bc8
mareikeheuer <mareike.heuer@wikimedia.de>
2023-12-07 16:00:08 +0100

    Codex: additional tests for CategoriesSection
    Increased test coverage.
    Added more assertions for visible and hidden category rendering to validate the functioning integration between parent (CategoriesSection) and child (CategoriesList) components.
    
    Bug: T350798
    Change-Id: I07b686eaf838092f983a6d45cc7d32682ef36081

--- tests/vue/CategoriesSection.test.js
+++ tests/vue/CategoriesSection.test.js
@@ -17,7 +17,8 @@ global.mw = {
 // Mock  CategoriesList
 jest.mock( '../../modules/components/CategoriesList.vue', () => ( {
 	name: 'CategoriesList',
-	template: '<div></div>'
+	template: '<div><ul><li v-for="category in categories">{{ category }}</li></ul></div>',
+	props: [ 'categories' ]
 } ) );
 
 const CategoriesSection = require( '../../modules/components/CategoriesSection.vue' );
@@ -33,7 +34,7 @@ describe( 'CategoriesSection', () => {
 
 		expect( wrapper.find( '.cdx-message--notice' ).isVisible() ).toBeTruthy();
 
-		// Assert that the notice message is rendered with correct message key
+		// Notice message is rendered with correct message key
 		const messageContent = wrapper.find( '.cdx-message--notice' ).text();
 		expect( messageContent ).toBe( 'fileimporter-category-encouragement' );
 	} );
@@ -49,7 +50,7 @@ describe( 'CategoriesSection', () => {
 		expect( wrapper.find( '.catlinks' ).exists() ).toBeFalsy();
 	} );
 
-	it( 'displays category catlinks when present', async () => {
+	it( 'renders visible and hidden categories in distinct sections with correct content when present', async () => {
 		const wrapper = mount( CategoriesSection, {
 			props: {
 				visibleCategories: [ 'Animals', 'Nature' ],
@@ -57,6 +58,27 @@ describe( 'CategoriesSection', () => {
 			}
 		} );
 
-		expect( wrapper.find( '.catlinks' ).isVisible() ).toBeTruthy();
+		const catlinksWrapper = wrapper.find( '.catlinks' );
+		expect( catlinksWrapper.isVisible() ).toBeTruthy();
+
+		// Normal and hidden categories are located inside the catlinksWrapper
+		const normalCategoriesWrapper = catlinksWrapper.find( '.mw-normal-catlinks' );
+		expect( normalCategoriesWrapper.isVisible() ).toBeTruthy();
+
+		const hiddenCategoriesWrapper = catlinksWrapper.find( '.mw-hidden-catlinks' );
+		expect( hiddenCategoriesWrapper.isVisible() ).toBeTruthy();
+
+		// Correct number of list items and text values for visible categories
+		const visibleCategoriesListItems = normalCategoriesWrapper.findAll( 'li' );
+
+		expect( visibleCategoriesListItems ).toHaveLength( 2 );
+		expect( visibleCategoriesListItems[ 0 ].text() ).toBe( 'Animals' );
+		expect( visibleCategoriesListItems[ 1 ].text() ).toBe( 'Nature' );
+
+		// Correct number of list items and text values for hidden categories
+		const hiddenCategoriesListItems = hiddenCategoriesWrapper.findAll( 'li' );
+
+		expect( hiddenCategoriesListItems ).toHaveLength( 1 );
+		expect( hiddenCategoriesListItems[ 0 ].text() ).toBe( 'Hiddencategory' );
 	} );
 } );
