0a5e29aef47b65ef5dba74b4eca965a3565c1f07
Adam Wight <adam.wight@wikimedia.de>
2024-01-17 07:53:07 +0100

    Drop Vue 2 forward compatibility
    This is no longer needed.
    
    Bug: T289105
    Change-Id: Ic2d9953372c6505dd88f31ba225ad45add4b02dc

--- modules/ext.FileImporter.SpecialCodex.js
+++ modules/ext.FileImporter.SpecialCodex.js
@@ -4,12 +4,6 @@ const launchVueApp = function () {
 	const Vue = require( 'vue' );
 	const App = require( './components/App.vue' );
 
-	if ( Vue.configureCompat ) {
-		Vue.configureCompat( {
-			MODE: 3
-		} );
-	}
-
 	Vue.createMwApp( App )
 		.mount( '#ext-fileimporter-vue-root' );
 };
